963fc776e82fd697913bfc3719f12a76084e5c2b
tulsee <ramtulsi022@gmail.com>
2024-01-12 16:59:07 +0545

    created hero component and footer component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div className="bg-blue-800 py-6">
-      <div className="mx-auto px-7 flex justify-between">
+      <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
           <Link to="/">MERN Hotel Booking</Link>
         </span>
